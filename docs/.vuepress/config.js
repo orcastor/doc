748247f30f4cd0c999247f0eca729860645b86e8
Orca <orca.zhang@yahoo.com>
2022-09-01 17:24:47 +0800

    :maple_leaf: update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,12 @@ module.exports = {
     title: 'OrcaS',
     description: '一款开箱即用的轻量级对象存储',
     base: "/doc/",
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ],
     themeConfig: {
+        logo: '/favicon.ico',
+
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
         repo: 'orcastor/orcas',
         // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
