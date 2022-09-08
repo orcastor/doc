804c49d5cedcc3d43ff4c1fa8f4c132ce908018a
Orca <orca.zhang@yahoo.com>
2022-09-08 22:09:30 +0800

    :flags: update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -63,7 +63,7 @@ module.exports = {
                 sidebar: {
                     "/orcas/": [
                         {
-                            title: "从0开始设计对象文件系统",
+                            title: "从0开始设计对象存储",
                             collapsable: false, // 可选的, 默认值是 true,
                             children: [
                                 "/orcas/",
